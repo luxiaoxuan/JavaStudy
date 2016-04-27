8a9b5e7f17c2264a7ee0d3fdbf20bf75cc5fc682
luxiaoxuan <luxiaoxuan1984@qq.com>
2016-04-27 11:56:25 +0800

    ....

--- src/test/java/LxxMaven/WebTest/NewTest3.java
+++ src/test/java/LxxMaven/WebTest/NewTest3.java
@@ -7,7 +7,8 @@ public class NewTest3 {
 	@Test(groups = { "Fail", })
 	public void makeError() {
 
-		int invalid = 100 / 0;
+//		int invalid = 100 / 0;
+		int invalid = 100 / 10;
 
 		System.out.println(invalid);
 	}
